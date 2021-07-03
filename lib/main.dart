cd0688aac7ff4b7554e0f4a13eb5e1783171114e
Shimon Iwata <shimon@ShimonnoMacBook-Air.local>
2021-07-03 21:28:44 +0900

    make under-bar

--- lib/main.dart
+++ lib/main.dart
@@ -2,24 +2,87 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-
 class MyApp extends StatelessWidget {
+  static const String _title = 'Flutter Code Sample';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('100'),
-        ),
-        body: Text(
-          'one-hundred',
-          style: TextStyle(
-            fontSize: 30,
+      title: _title,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // 表示中の Widget を取り出すための index としての int 型の mutable な stored property
+  int _selectedIndex = 0;
+
+  // 表示する Widget の一覧
+  static List<Widget> _pageList = [
+    CustomPage(pannelColor: Colors.cyan, title: 'Home'),
+    CustomPage(pannelColor: Colors.green, title: 'Settings'),
+    CustomPage(pannelColor: Colors.pink, title: 'Search')
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('100'),
+      ),
+      body: _pageList[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text('Home'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            title: Text('MyPage'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            title: Text('Settings'),
+          ),
+        ],
+        currentIndex: _selectedIndex,
+
+      ),
+    );
+  }
+}
+
+class CustomPage extends StatelessWidget {
+  final Color pannelColor;
+  final String title;
+
+  CustomPage({required this.pannelColor, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    final titleTextStyle = Theme.of(context).textTheme.title;
+    return Container(
+      child: Center(
+        child: Container(
+          width: 200,
+          height: 200,
+          decoration: BoxDecoration(
+              color: pannelColor,
+              borderRadius: BorderRadius.all(Radius.circular(20.0))),
+          child: Center(
+            child: Text(
+              title,
+            ),
           ),
         ),
       ),
     );
   }
 }
-
