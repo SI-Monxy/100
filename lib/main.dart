52480ab9690fe4b5a1d2d11f77a727848b27daf9
もんくし <68159415+monxy747@users.noreply.github.com>
2021-07-10 21:26:48 +0900

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:one_hundred/presentation/top/top_page.dart';
 
 void main() {
-  // main内で非同期処理を呼び出す場合runApp前に初期化が必要
-  WidgetsFlutterBinding.ensureInitialized();
-
+  runApp(MaterialApp(home: TopPage(),),);
 }
